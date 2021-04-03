0ffcafc1a0c99cdf73f07b2b2e2edb9a43304043
adilsonchameia <adilsonkchameia@gmail.com>
2021-04-03 15:31:00 +0100

    banco de dados criado

--- lib/main.dart
+++ lib/main.dart
@@ -16,23 +16,28 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   //Metodo para recuperar os dados
-  _recuperarDados() async {
-    final caminhoBancoDados =
-        await getDatabasesPath(); //Recuperar o caminho para ambas plantaformas
-    final localBancoDados = join(caminhoBancoDados, "banco.db"); //Nome do banco
+  _recuperarBancoDados() async {
+    //Recuperar o caminho para ambas plantaformas
+    final caminhoBancoDados = await getDatabasesPath();
+    //Nome do banco e caminho do banco (exe: caminhoBancoDados/banco.db)
+    final localBancoDados = join(caminhoBancoDados, "banco.db");
     //Iniciar a conexao com o banco de dados
-    openDatabase(localBancoDados, version: 1, onCreate: (db, dbVersaoRecente) {
-      //db consigo fazer alteracoes para quando atualizar.
+    var retorno = await openDatabase(localBancoDados, version: 1,
+        //db consigo fazer alteracoes para quando atualizar.
+        onCreate: (db, dbVersaoRecente) {
       //Exemplo de uma sql habitual de criar tabela
       String sql =
           "CREATE TABLE usuario (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INTEGER) ";
       //Executar a sql criada.
       db.execute(sql);
     });
+
+    print("Aberto: " + retorno.isOpen.toString());
   }
 
   @override
   Widget build(BuildContext context) {
+    _recuperarBancoDados();
     return Scaffold(
       appBar: AppBar(
         title: Text("SQLFlite Config"),
