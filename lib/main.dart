f563cd63608818a48125164219b67e324ae3ba9d
adilsonchameia <adilsonkchameia@gmail.com>
2021-04-03 15:24:16 +0100

    criacao do metodo de criar o banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -14,6 +15,22 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  //Metodo para recuperar os dados
+  _recuperarDados() async {
+    final caminhoBancoDados =
+        await getDatabasesPath(); //Recuperar o caminho para ambas plantaformas
+    final localBancoDados = join(caminhoBancoDados, "banco.db"); //Nome do banco
+    //Iniciar a conexao com o banco de dados
+    openDatabase(localBancoDados, version: 1, onCreate: (db, dbVersaoRecente) {
+      //db consigo fazer alteracoes para quando atualizar.
+      //Exemplo de uma sql habitual de criar tabela
+      String sql =
+          "CREATE TABLE usuario (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VARCHAR, idade INTEGER) ";
+      //Executar a sql criada.
+      db.execute(sql);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
